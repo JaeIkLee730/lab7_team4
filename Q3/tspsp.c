e0112637c5a68145a526a8990d0d8513d063977a
JaeIkLee <21200574@handong.edu>
2017-06-02 13:55:35 +0900

    change for loop into to sequential lines

--- Q3/tspsp.c
+++ Q3/tspsp.c
@@ -8,9 +8,12 @@ static pid_t pid;
 
 void handler(int signum)
 {
-        int s;
-        kill(pid, SIGINT);
-        wait(&s);
+        int s,i;
+	for(i = 0; i < 17; i++)
+	{
+        	kill(pid, SIGINT);
+       		wait(&s);
+	}
         printf("ByeByeBye.\n");
 }
 
@@ -23,21 +26,118 @@ void main ()
 
         printf("Let's execute tsp.\n");
 	
-	for (i = 0; i < 17; i++)
-	{
+//	for (i = 0; i < 17; i++)
+//	{
        	    if ((pid = fork()) == 0)
             {
-                    execl("./tsp", "tsp", "i", NULL);
+                    execl("./tsp", "tsp", "0", NULL);
             }
-        }
+  //      }
 
-        for (i = 0; i < 17; i++)
-        {
+       // for (i = 0; i < 17; i++)
+      //  {
+        //    if ((pid = fork()) == 0)
+          //  {
+	//	waitpid(pid, &s, 0);
+          //  }
+      //  }
             if ((pid = fork()) == 0)
             {
-		waitpid(pid, &s, 0);
+                    execl("./tsp", "tsp", "1", NULL);
             }
-        }
+            
+	    if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "2", NULL);
+            }
+
+	    if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "3", NULL);
+            }
+
+	    if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "4", NULL);
+            }
+
+	    if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "5", NULL);
+            }
+
+	    if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "6", NULL);
+            }
+
+	    if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "7", NULL);
+            }
+
+ 	    if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "8", NULL);
+            }
+
+            if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "9", NULL);
+            }
+
+            if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "10", NULL);
+            }
+
+            if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "11", NULL);
+            }
+           
+	     if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "12", NULL);
+            }
+
+            if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "13", NULL);
+            }
+
+            if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "14", NULL);
+            }
+
+            if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "15", NULL);
+            }
+
+            if ((pid = fork()) == 0)
+            {
+                    execl("./tsp", "tsp", "16", NULL);
+            }
+
+            waitpid(pid, &s, 0);
+	    waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
+            waitpid(pid, &s, 0);
 
 	exit(0);
 }
